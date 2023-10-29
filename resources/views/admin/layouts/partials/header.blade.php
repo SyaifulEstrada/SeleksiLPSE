2f24c346da3d1f383ef5ac6acefaf4c4391d4604
SyaifulEstrada <syaifultheminister@gmail.com>
2023-10-29 22:11:03 +0700

    Menambahkan test code

--- resources/views/admin/layouts/partials/header.blade.php
+++ resources/views/admin/layouts/partials/header.blade.php
@@ -4,6 +4,7 @@
         data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
+    <input class="form-control form-control-dark w-100" type="text" placeholder="Search" readonly>
     <ul class="navbar-nav">
         <li class="nav-item">
             <form action="/logout" method="POST">
