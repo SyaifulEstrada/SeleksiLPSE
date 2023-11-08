90862d2a564fcb06d064cfc68aae012ca1f34cce
SyaifulEstrada <syaifultheminister@gmail.com>
2023-11-08 09:44:15 +0700

    done.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -45,6 +45,7 @@
             </div>
         </div>
     </div>
+    
     <footer class="bg-dark text-center text-white">
         <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
             © 2023 Copyright:
